71535e8988179a58b0ae1e8d1f2a32bbaca6038f
hooshyar <hooseyr@gmail.com>
2018-12-01 17:34:52 +0330

    Before Wp-Client

--- lib/src/blocs/serialize.dart
+++ lib/src/blocs/serialize.dart
@@ -1,8 +1,5 @@
 import 'package:json_annotation/json_annotation.dart';
-part 'post.g.dart';
 
-
-@JsonSerializable()
 class Post {
   int _id;
   String _title;
@@ -16,5 +13,5 @@ class Post {
   Post(this._id, this._title, this._content, this._author, this._date,
       [this._imgUrl]);
 
-  factory Post.fromJson(Map<String, dynamic> json) => _$PostFromJson(json);
+
 }
